422dd4d5e3d988d375aa683affc43b544f0d3b86
apple95 <rajaa@roe-hulman.edu>
2015-02-08 19:36:07 -0500

    Created SCORE cordite

---
+++ GoLeft/Score.m
@@ -0,0 +1,17 @@
+//
+//  Score.m
+//  GoLeft
+//
+//  Created by Ashok Vardhan Raja on 2/8/15.
+//  Copyright (c) 2015 Philip Ross. All rights reserved.
+//
+
+#import "Score.h"
+
+
+@implementation Score
+
+@dynamic score;
+@dynamic userName;
+
+@end
