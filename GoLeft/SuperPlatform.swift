cbbe5c8cb7e68c37b5ce04ad2bedd48de16e5f3e
Philip Ross <rosspa@rose-hulman.edu>
2015-02-09 16:43:19 -0500

    moved stuff from BrickPlatform to SuperPlatform

--- GoLeft/SuperPlatform.swift
+++ GoLeft/SuperPlatform.swift
@@ -9,6 +9,9 @@
 import UIKit
 import SpriteKit
 
+protocol RequiredPlatform {
+    func initPhysics();
+}
 class SuperPlatform: SKSpriteNode {
 
     init(imageName: (String)) {
@@ -17,8 +20,50 @@ class SuperPlatform: SKSpriteNode {
         
     }
     
+    // specify the location
+    init(imageName: (String), length: (CGFloat), x: (CGFloat), y: (CGFloat)) {
+        let texture = SKTexture(imageNamed: imageName)
+        super.init(texture: texture, color: UIColor.clearColor(), size: texture.size())
+        
+        //        self.size = CGSize(width: self.size.width * length, height: self.size.height)
+        self.xScale = length
+        self.position = CGPoint(x: x+self.size.width / 2, y: y-self.size.height/2)
+        
+        initPhysics()
+    }
+    
+    init(imageName: (String), length: (CGFloat)) {
+        let texture = SKTexture(imageNamed: imageName)
+        super.init(texture: texture, color: UIColor.clearColor(), size: texture.size())
+        //        self.size = CGSize(width: self.size.width * length, height: self.size.height)
+        self.xScale = length
+        let bounds = UIScreen.mainScreen().bounds
+        let screenWidth = bounds.width
+        let screenHeight = bounds.height
+        
+        
+        let x = random(min: -self.size.width, max: screenWidth)
+        let y = random(min: -self.size.height, max: screenHeight)
+        
+        self.position = CGPoint(x: x+self.size.width/2, y: y-self.size.height/2)
+        initPhysics()
+    }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random())/0xFFFFFFFF)
+    }
+    
+    func random(#min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func initPhysics() {
+        assert(false, "This method must be overridden")
+    }
+
+    
 
 }
