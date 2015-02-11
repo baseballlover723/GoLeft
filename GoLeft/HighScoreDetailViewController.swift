cf16a0afba520349de1a138ba550dd9f5f8fc2e2
apple95 <rajaa@roe-hulman.edu>
2015-02-11 16:37:34 -0500

    Created MasterViewController and Detail ViewController

---
+++ GoLeft/HighScoreDetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  jkj
+//
+//  Created by Ashok Vardhan Raja on 2/11/15.
+//  Copyright (c) 2015 Rose-Hulman. All rights reserved.
+//
+
+import UIKit
+
+class HighScoreDetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail: AnyObject = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.valueForKey("timeStamp")!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
