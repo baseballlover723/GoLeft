8bdfc8036ad3d21ca047412ab3dff62c8538beca
apple95 <rajaa@roe-hulman.edu>
2015-02-14 14:27:18 -0500

    HighScore Stuff using file

--- GoLeft/HighScoreDetailViewController.swift
+++ GoLeft/HighScoreDetailViewController.swift
@@ -8,38 +8,84 @@
 
 import UIKit
 
-class HighScoreDetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    var detailItem: AnyObject? {
-        didSet {
-            // Update the view.
-            self.configureView()
-        }
-    }
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail: AnyObject = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.valueForKey("timeStamp")!.description
+//class HighScoreDetailViewController: UIViewController {
+//
+//    @IBOutlet weak var detailDescriptionLabel: UILabel!
+//
+//
+//    var detailItem: AnyObject? {
+//        didSet {
+//            // Update the view.
+//            self.configureView()
+//        }
+//    }
+//
+//    func configureView() {
+//        // Update the user interface for the detail item.
+//        if let detail: AnyObject = self.detailItem {
+//            if let label = self.detailDescriptionLabel {
+//                label.text = detail.valueForKey("timeStamp")!.description
+//            }
+//        }
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//        self.configureView()
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+//
+//}
+class HighScoreManager {
+    var scores:Array<HighScore> = [];
+    
+    init() {
+        // load existing high scores or set up an empty array
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentsDirectory = paths[0] as String
+        let path = documentsDirectory.stringByAppendingPathComponent("HighScores.plist")
+        let fileManager = NSFileManager.defaultManager()
+        
+        // check if file exists
+        if !fileManager.fileExistsAtPath(path) {
+            // create an empty file if it doesn't exist
+            if let bundle = NSBundle.mainBundle().pathForResource("DefaultFile", ofType: "plist") {
+                fileManager.copyItemAtPath(bundle, toPath: path, error:nil)
             }
         }
+        
+        if let rawData = NSData(contentsOfFile: path) {
+            // do we get serialized data back from the attempted path?
+            // if so, unarchive it into an AnyObject, and then convert to an array of HighScores, if possible
+            var scoreArray: AnyObject? = NSKeyedUnarchiver.unarchiveObjectWithData(rawData);
+            self.scores = scoreArray as? [HighScore] ?? [];
+        }
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.configureView()
+    
+    func save() {
+        // find the save directory our app has permission to use, and save the serialized version of self.scores - the HighScores array.
+        let saveData = NSKeyedArchiver.archivedDataWithRootObject(self.scores);
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as NSArray;
+        let documentsDirectory = paths.objectAtIndex(0) as NSString;
+        let path = documentsDirectory.stringByAppendingPathComponent("HighScores.plist");
+        
+        saveData.writeToFile(path, atomically: true);
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // a simple function to add a new high score, to be called from your game logic
+    // note that this doesn't sort or filter the scores in any way
+    func addNewScore(newScore:Int) {
+        let newHighScore = HighScore(score: newScore, dateOfScore: NSDate());
+        self.scores.append(newHighScore);
+        self.save();
     }
-
-
 }
+
+
 
